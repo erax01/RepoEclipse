aa0770581cfb30aa542fda09714d80cc815cd2bd
Jordie Rieder <jrieder@student.tgm.ac.at>
2018-02-27 13:40:28 +0100

    Test.java has been edited (Hallo 3x)

--- TestMerge/src/Test.java
+++ TestMerge/src/Test.java
@@ -1,6 +1,8 @@
 
 public class Test {
 	public static void Hallo() {
-		System.out.println("Hallo");
+		System.out.println("Hallo1");
+		System.out.println("Hallo2");
+		System.out.println("Hallo3");	
 	}
 }
